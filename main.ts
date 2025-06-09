cd818267864dc3d6955d968963f48665fe9e12df
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2025-06-08 19:57:06 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,39 +1,95 @@
 input.onButtonPressed(Button.A, function () {
-    loc += -1
+    loc = (loc + 99) % 100
+    led.unplot(1, 4)
+    led.unplot(1, 3)
+    showLND(loc)
 })
+function hitskull (locat: number) {
+    cur = (locat + 1) % 100
+    skl[cur] = 0
+    showLND(locat)
+}
 function mkLand () {
     lnd = []
     for (let index = 0; index < 100; index++) {
         lnd.push(randint(0, 2))
     }
     skl = []
-    for (let index = 0; index < 5; index++) {
-        skl.push(randint(0, 99))
+    for (let index = 0; index < 100; index++) {
+        if (8 < randint(0, 20)) {
+            skl.push(1)
+        } else {
+            skl.push(0)
+        }
     }
 }
-input.onButtonPressed(Button.B, function () {
-    loc += 1
+input.onButtonPressed(Button.AB, function () {
+    if (chkSkull(loc)) {
+        hitskull(loc)
+        basic.showIcon(IconNames.Target)
+        basic.pause(100)
+        basic.showIcon(IconNames.Diamond)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . # . . .
+            . . . . .
+            `)
+        game.addScore(5 * randint(3, 7))
+        showLND(loc)
+    }
+    showLND(loc)
 })
+input.onButtonPressed(Button.B, function () {
+    loc = (loc + 101) % 100
+    led.unplot(1, 4)
+    led.unplot(1, 3)
+    showLND(loc)
+})
+function chkSkull (locat: number) {
+    cur = (locat + 1) % 100
+    if (skl[cur] == 0) {
+        return false
+    } else {
+        return true
+    }
+}
 function showLND (locat: number) {
     for (let index = 0; index <= 4; index++) {
         spot = lnd[(locat + index) % 100]
+        skspot = skl[(locat + index) % 100]
         if (spot == 2) {
-            led.plot(index, 3)
+            led.plotBrightness(index, 3, 47)
         }
         if (spot > 0) {
-            led.plot(index, 4)
+            led.plotBrightness(index, 4, 47)
         } else {
             led.unplot(index, 4)
             led.unplot(index, 3)
         }
+        if (skspot != 0) {
+            led.plotBrightness(index, 0, 141)
+        } else {
+            led.unplot(index, 0)
+        }
     }
+    led.plot(1, 3)
+    led.plot(1, 4)
 }
+let skspot = 0
 let spot = 0
-let skl: number[] = []
 let lnd: number[] = []
-mkLand()
+let skl: number[] = []
+let cur = 0
 let loc = 0
-basic.showIcon(IconNames.Heart)
+game.setLife(5)
+mkLand()
+loc = 0
+basic.showIcon(IconNames.TShirt)
+basic.pause(100)
+basic.showIcon(IconNames.Sword)
 basic.pause(100)
 basic.showLeds(`
     . . . . .
@@ -42,6 +98,12 @@ basic.showLeds(`
     . . . . .
     . . . . .
     `)
+showLND(loc)
 basic.forever(function () {
-    showLND(loc)
+    basic.pause(1000 * (1 + randint(0, 3)))
+    if (chkSkull(loc)) {
+        game.removeLife(1)
+        showLND(loc)
+        basic.pause(2000)
+    }
 })
